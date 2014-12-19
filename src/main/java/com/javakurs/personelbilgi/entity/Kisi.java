30d805cc06d25d6d9e9a15a63acf8dcd67f9c722
dream <dream@192.168.2.16>
2014-12-19 10:56:24 +0200

    Entity sınıfları oluşturuldu.

---
+++ src/main/java/com/javakurs/personelbilgi/entity/Kisi.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javakurs.personelbilgi.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author dream
+ */
+@Entity
+@Table(catalog = "personelbilgi", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Kisi.findAll", query = "SELECT k FROM Kisi k"),
+    @NamedQuery(name = "Kisi.findById", query = "SELECT k FROM Kisi k WHERE k.id = :id"),
+    @NamedQuery(name = "Kisi.findByAd", query = "SELECT k FROM Kisi k WHERE k.ad = :ad"),
+    @NamedQuery(name = "Kisi.findBySoyad", query = "SELECT k FROM Kisi k WHERE k.soyad = :soyad")})
+public class Kisi implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(nullable = false, length = 20)
+    private String ad;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(nullable = false, length = 20)
+    private String soyad;
+    @OneToMany(mappedBy = "kisiId")
+    private List<Telefon> telefonList;
+
+    public Kisi() {
+    }
+
+    public Kisi(Integer id) {
+        this.id = id;
+    }
+
+    public Kisi(Integer id, String ad, String soyad) {
+        this.id = id;
+        this.ad = ad;
+        this.soyad = soyad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    @XmlTransient
+    public List<Telefon> getTelefonList() {
+        return telefonList;
+    }
+
+    public void setTelefonList(List<Telefon> telefonList) {
+        this.telefonList = telefonList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Kisi)) {
+            return false;
+        }
+        Kisi other = (Kisi) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.javakurs.personelbilgi.entity.Kisi[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/com/javakurs/personelbilgi/entity/Telefon.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javakurs.personelbilgi.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author dream
+ */
+@Entity
+@Table(catalog = "personelbilgi", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Telefon.findAll", query = "SELECT t FROM Telefon t"),
+    @NamedQuery(name = "Telefon.findById", query = "SELECT t FROM Telefon t WHERE t.id = :id"),
+    @NamedQuery(name = "Telefon.findByTelNo", query = "SELECT t FROM Telefon t WHERE t.telNo = :telNo")})
+public class Telefon implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Size(max = 20)
+    @Column(name = "tel_no", length = 20)
+    private String telNo;
+    @JoinColumn(name = "kisi_id", referencedColumnName = "id")
+    @ManyToOne
+    private Kisi kisiId;
+
+    public Telefon() {
+    }
+
+    public Telefon(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    public Kisi getKisiId() {
+        return kisiId;
+    }
+
+    public void setKisiId(Kisi kisiId) {
+        this.kisiId = kisiId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Telefon)) {
+            return false;
+        }
+        Telefon other = (Telefon) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.javakurs.personelbilgi.entity.Telefon[ id=" + id + " ]";
+    }
+    
+}
